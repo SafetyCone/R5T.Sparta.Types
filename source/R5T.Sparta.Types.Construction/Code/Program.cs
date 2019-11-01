9ecfdf72799640b6079b4509227deb2221284866
MinexAutomation <minex.automation@gmail.com>
2019-11-01 14:10:32 -0700

    Initial commit of source-stub.

---
+++ source/R5T.Sparta.Types.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Sparta.Types.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ source/R5T.Sparta.Types/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Sparta.Types
+{
+    public class Class1
+    {
+    }
+}
