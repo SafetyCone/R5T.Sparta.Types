087bb79dfd41098f8f597cecfbfffa4e3f7bcd7b
MinexAutomation <minex.automation@gmail.com>
2019-11-07 18:08:21 -0800

    Added file-name type (TypedString).

---
+++ source/R5T.Sparta.Types/Code/Classes/FileName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using R5T.Stagira;
+
+
+namespace R5T.Sparta
+{
+    public class FileName : TypedString
+    {
+        public FileName(string value)
+            : base(value)
+        {
+        }
+    }
+}
