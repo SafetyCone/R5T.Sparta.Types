e5a1f3b80318653b0b7080d61fbc5f034f5a7f6a
MinexAutomation <minex.automation@gmail.com>
2019-11-11 00:34:51 -0800

    Added directory path type.

---
+++ source/R5T.Sparta.Types/Code/Classes/DirectoryPath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using R5T.Stagira;
+
+
+namespace R5T.Sparta
+{
+    public class DirectoryPath : TypedString
+    {
+        #region Static
+
+        public static DirectoryPath New(string directoryPath)
+        {
+            var output = new DirectoryPath(directoryPath);
+            return output;
+        }
+
+        #endregion
+
+
+        public DirectoryPath(string value)
+            : base(value)
+        {
+        }
+    }
+}
